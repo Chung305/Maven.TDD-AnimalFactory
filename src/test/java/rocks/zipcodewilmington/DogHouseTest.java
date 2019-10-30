a1a245f674bb499a2141e4743969f6c2122aaeb6
Chung Arguello <chung@zipcoders-MacBook-Pro-90.local>
2019-10-30 16:07:57 -0400

    removeDogByIdTest added

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,5 +1,6 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
@@ -33,6 +34,23 @@ public class DogHouseTest {
     }
 
     @Test
+    public void getDogByIdTest(){
+        String name = "Milo";
+        Date birthDate = new Date();
+        DogHouse.clear();
+
+        Dog animal = AnimalFactory.createDog(name, birthDate);
+        Integer expected = DogHouse.getNumberOfDogs() + 1;
+
+        DogHouse.add(animal);
+
+
+        Assert.assertEquals(expected, DogHouse.getDogById(1));
+
+
+    }
+
+    @Test
     public void testRemoveDogs() {
         // Given (some
         String name = "Milo";
@@ -56,10 +74,33 @@ public class DogHouseTest {
         DogHouse.clear();
 
         // When
-        DogHouse.add(animal);
 
+        Integer expected =  DogHouse.getNumberOfDogs() + 1;
+        DogHouse.add(animal);
         // Then
-        DogHouse.getNumberOfDogs();
+        Assert.assertEquals(expected, DogHouse.getNumberOfDogs());
+
+    }
+    @Test
+    public void removeDogByIdTest(){
+        String name = "Milo";
+        Date birthDate = new Date();
+        Dog animal = AnimalFactory.createDog(name, birthDate);
+        DogHouse.clear();
+
+
+        Integer expected = DogHouse.getNumberOfDogs();
+        DogHouse.add(animal);
+        DogHouse.remove(0);
+
+
+
+        Assert.assertEquals(expected, DogHouse.getNumberOfDogs(),0);
+
+//        Dog dog = new Dog("chung", new Date(), 2);
+//        DogHouse.add(dog);
+//        DogHouse.remove(dog.getId());
+//        Assert.assertEquals(null, DogHouse.getDogById(dog.getId()));
     }
 }
 
